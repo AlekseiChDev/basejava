2ad0f310e87e8f8d7a1306c965b42c64a0149b9a
AlekseiCh <AlekseiChDevB@gmail.com>
2025-01-27 12:13:49 +0300

    Lesson03 abstract storage & sorted storage

--- src/ru/javawebinar/basejava/storage/ArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/ArrayStorage.java
@@ -4,78 +4,49 @@ package ru.javawebinar.basejava.storage; /**
 import ru.javawebinar.basejava.model.Resume;
 
 import java.util.Arrays;
-public class ArrayStorage {
-    private static final int STORAGE_LIMIT = 10000;
-    private final Resume[] storage = new Resume[STORAGE_LIMIT];
-    private int size;
+public class ArrayStorage extends AbstractArrayStorage {
 
     public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    public void save(Resume r) {
-        int index = findIndex(r.getUuid());
-        if (index != -1) {
-            System.out.println("Ошибка сохранения: резюме " + r + " уже есть в хранилище. ");
-            return;
-        }
-
-        if (size == storage.length) {
-            System.out.println("Ошибка сохранения: хранилище переполнено");
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index == -1) {
+            System.out.println("Resume " + r.getUuid() + " not exist");
         } else {
-            storage[size] = r;
-            size++;
-            System.out.println("Резюме " + r + " сохранено успешно");
+            storage[index] = r;
         }
     }
 
-    public Resume get(String uuid) {
-        int index = findIndex(uuid);
-        if (index == -1) {
-            System.out.println("Резюме " + uuid + " не найдено");
-            return null;
+    public void save(Resume r) {
+        if (getIndex(r.getUuid()) != -1) {
+            System.out.println("Resume " + r.getUuid() + " already exist");
+        } else if (size >= STORAGE_LIMIT) {
+            System.out.println("Storage overflow");
         } else {
-            return storage[index];
+            storage[size] = r;
+            size++;
         }
     }
 
     public void delete(String uuid) {
-        int index = findIndex(uuid);
+        int index = getIndex(uuid);
         if (index == -1) {
-            System.out.println("Резюме " + uuid + " не найдено");
+            System.out.println("Resume " + uuid + " not exist");
         } else {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
             size--;
-            if (index != size) {
-                storage[index] = storage[size];
-            }
-            storage[size] = null;
-            System.out.println("Резюме " + uuid + " удалено");
         }
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
     public Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
-    public int size() {
-        return size;
-    }
-
-    public void update(Resume resume) {
-        int index = findIndex(resume.getUuid());
-        if (index == -1) {
-            System.out.println("Ошибка обновления: резюме " + resume + " еще нет в хранилище. ");
-        } else {
-            storage[index] = resume;
-            System.out.println("Резюме " + resume + " обновлено");
-        }
-    }
-
-    private int findIndex(String uuid) {
+    protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].getUuid())) {
                 return i;
