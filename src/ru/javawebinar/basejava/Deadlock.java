3280d90c4c5865fbd4963f68779ef8f032da4360
AlekseiCh <AlekseiChDevB@gmail.com>
2025-04-07 14:00:36 +0300

    HW11 correction 01

--- src/ru/javawebinar/basejava/Deadlock.java
+++ src/ru/javawebinar/basejava/Deadlock.java
@@ -2,38 +2,26 @@ package ru.javawebinar.basejava;
 
 public class Deadlock {
     public static void main(String[] args) {
-        final Object lock1 = new Object();
-        final Object lock2 = new Object();
+        final String lock1 = "lock1";
+        final String lock2 = "lock2";
+        lock(lock1, lock2);
+        lock(lock2, lock1);
+    }
 
-        Thread thread1 = new Thread(() -> {
+    private static void lock(String lock1, String lock2) {
+        Thread thread = new Thread(() -> {
             System.out.println(Thread.currentThread().getName() + ": " + Thread.currentThread().getState());
             synchronized (lock1) {
-                System.out.println(Thread.currentThread().getName() + ": holds lock1");
+                System.out.println(Thread.currentThread().getName() + ": holds " + lock1);
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException ignored) {}
-                System.out.println(Thread.currentThread().getName() + ": wait lock2");
+                System.out.println(Thread.currentThread().getName() + ": wait " + lock2);
                 synchronized (lock2) {
-                    System.out.println(Thread.currentThread().getName() + ": holds  lock1, lock2");
+                    System.out.println(Thread.currentThread().getName() + ": holds " + lock2);
                 }
             }
         });
-
-        Thread thread2 = new Thread(() -> {
-            System.out.println(Thread.currentThread().getName() + ": " + Thread.currentThread().getState());
-            synchronized (lock2) {
-                System.out.println(Thread.currentThread().getName() + ": holds lock2");
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException ignored) {}
-                System.out.println(Thread.currentThread().getName() + ": wait lock1");
-                synchronized (lock1) {
-                    System.out.println(Thread.currentThread().getName() + ": holds  lock1, lock2");
-                }
-            }
-        });
-
-        thread1.start();
-        thread2.start();
+        thread.start();
     }
 }
